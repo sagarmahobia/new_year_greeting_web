58a1a2d1917746a80191683cd5844336544714d6
Sagar Mahobia <sagarmahobia5@gmail.com>
2017-12-25 20:39:44 +0530

    fixed crete new workflow

--- myscript.js
+++ myscript.js
@@ -87,17 +87,18 @@ function onClickCreateNew() {
     var inputInAdvanced = $("#input-in-advanced").is(":checked");
     var url = generateUrl(inputName, inputInAdvanced);
     if (url != null) {
-        setName(inputName);
-        setTitle(inputName);
-        if (inputInAdvanced) {
-            showInAdvanced();
+        // setName(inputName);
+        // setTitle(inputName);
+        // if (inputInAdvanced) {
+        //     showInAdvanced();
 
-        } else {
-            hideInAdvanced();
-        }
+        // } else {
+        //     hideInAdvanced();
+        // }
 
-        showSharer();
-        generatedUrl = url;
+        // showSharer();
+         generatedUrl = url;
+        window.location  = url;
     }
 }
 
